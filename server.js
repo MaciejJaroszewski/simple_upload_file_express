d3d644d44353566e0946ffa0ad3ac469db351375
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-04-03 23:14:36 +0200

    Add necessary dependencies

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: __dirname + '/public/uploads' });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/public`));
 
